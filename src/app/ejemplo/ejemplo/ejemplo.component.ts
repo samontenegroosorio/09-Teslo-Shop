112c4c0caf45a7ae6afcc7097e0cad41ef870be7
Sebastián Montenegro <samontenegroosorio@gmail.com>
2025-05-15 20:06:16 +0200

    add EjemploComponent with initial template and structure

---
+++ src/app/ejemplo/ejemplo/ejemplo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo',
+  imports: [],
+  templateUrl: './ejemplo.component.html',
+})
+export class EjemploComponent { }
